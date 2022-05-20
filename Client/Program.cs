0708252c9acb351203cd6bb9bc21657aea35e63a
Joshua Donovan <Josh@alcanweb.com>
2022-05-20 18:31:46 -0500

    Its working!

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,19 @@
 using BlazorBasic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
+using System.Net.Http.Json;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => 
+    new HttpClient
+    {
+        BaseAddress = new Uri("https://sfinternetoutage.azurewebsites.net/")
 
+    });
 await builder.Build().RunAsync();
